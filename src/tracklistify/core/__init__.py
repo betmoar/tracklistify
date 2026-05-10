c12837110b45e993dc60c0441d787e7f202fbc47
betmoar <betmoar@mailsecure.me>
2026-05-10 17:13:28 +0200

    fix(core): import ApplicationError from exceptions module

--- src/tracklistify/core/__init__.py
+++ src/tracklistify/core/__init__.py
@@ -3,7 +3,8 @@ This module initializes the core components of the tracklistify package.
 """
 
 # Local/package imports (using relative imports at package root)
-from .base import ApplicationError, AsyncApp
+from .base import AsyncApp
+from .exceptions import ApplicationError
 from .track import Track, TrackMatcher
 from .types import ProviderResponse, TrackIdentificationProvider, TrackMetadata
 
